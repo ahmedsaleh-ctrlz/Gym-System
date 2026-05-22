e747fe32d6331a811fdad7d07eaefb8c262f56b4
Ahmed <ahmedsalehx11@gmail.com>
2026-05-22 22:44:04 +0300

    Removed a client

--- src/Gym.Client/Models/PaginatedList.cs
+++
@@ -1,9 +0,0 @@
-namespace Gym.Client.Models;
-
-public sealed class PaginatedList<T>
-{
-    public int PageNumber { get; set; }
-    public int PageSize { get; set; }
-    public int TotalCount { get; set; }
-    public IReadOnlyCollection<T>? Items { get; set; }
-}
--- src/Gym.Client/Models/ProblemResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Gym.Client.Models;
-
-public sealed class ProblemResponse
-{
-    public string? Title { get; set; }
-    public string? Detail { get; set; }
-    public int? Status { get; set; }
-}
